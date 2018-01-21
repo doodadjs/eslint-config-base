92649d92d0decb8494a64eb007f8b79ceb0187eb
Claude Petit <doodadjs@gmail.com>
2018-01-21 17:41:17 -0500

    Apply same rules to the package itself.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,26 @@
 module.exports = {
-	"extends": "airbnb-base",
+	env: {
+		browser: true,
+		commonjs: true,
+		es6: true
+	},
 
-	"rules": {
+	plugins: [
+		"import"
+	],
+
+	extends: [
+		'airbnb-base',
+	].concat([
+		'./rules/overrides.js',
+		'./rules/temporary.js',
+	].map(require.resolve)),
+
+	parserOptions: {
+		ecmaVersion: 2017,
+		sourceType: 'cjs',
+		ecmaFeatures: {
+			experimentalObjectRestSpread: true,
+		},
 	},
 };
