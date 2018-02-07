5e20cf955d9ad0ea2f5f820fc1be9f0d4ddf8264
Claude Petit <doodadjs@gmail.com>
2018-02-07 17:38:44 -0500

    Re-enable "guard-for-in".

--- rules/temporary.js
+++ rules/temporary.js
@@ -24,8 +24,5 @@ module.exports = {
 		"prefer-destructuring": 'off',
 
 		"prefer-spread": 'off',
-		
-		// False positive on Bootstrap:373 (array-like section of "types.keys")
-		"guard-for-in" : "off",
 	},
 };
