3d5fa0d6896fab814ac9a94c824fb77201060953
Claude Petit <cpetit636@gmail.com>
2026-08-02 09:40:10 -0400

    review rules

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-module.exports = require("./.eslintrc.js");
+const js = require('@eslint/js');
+
+module.exports = {
+    configs: {
+        recommended: {
+            rules: {
+                ...js.configs.recommended.rules,
+                ...require('./rules/overrides'),
+                ...require('./rules/temporary'),
+            },
+        },
+    },
+};
--- rules/temporary.js
+++ rules/temporary.js
@@ -1,24 +1,2 @@
 module.exports = {
-  rules: {
-	  	// DD-DOC
-		"comma-style": 'off',
-
-		"func-names": 'off',
-
-		// Maybe change to "never" later...
-		"function-paren-newline": 'off',
-
-		"no-nested-ternary": 'off',
-
-		"no-unneeded-ternary": 'off',
-
-		"object-shorthand": 'off',
-
-		// We place our operators at the end
-		"operator-linebreak": ['error', 'after'],
-
-		"prefer-destructuring": 'off',
-
-		"prefer-spread": 'off',
-	},
 };
